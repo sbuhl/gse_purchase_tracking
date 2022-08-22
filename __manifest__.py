d25b2f6dc49670d232194500c888f09dd8e8e939
shortcut1337 <really_mad@hotmail.com>
2022-08-22 19:28:50 +0200

    [FIX] remove warnings in log error due to incorrect rst

--- __manifest__.py
+++ __manifest__.py
@@ -8,49 +8,53 @@
         their position in the purchasing flow""",
 
     'description': """
-        GoShop, GoShop Energy, Kivu Solutions and Utrade, can make two 
-        kind of purchases: 
-            - Direct Purchases
-            - Indirect Purchases
-        Direct Purchases are done to the final Supplier. 
+        GoShop, GoShop Energy, Kivu Solutions and Utrade, can make two
+        kind of purchases:
+        - Direct Purchases
+        - Indirect Purchases
+        Direct Purchases are done to the final Supplier.
         Indirect Purchases are made throught the belgian company, Atimex
 
         Direct Purchase Flow is: GoShop -> BYD
         Indirect Purchase Flow is: GoShop -> Atimex -> Victron
 
         The important information for GoShop is when the marchandise will
-        be delivered. That's the reason why we introduce the notion of 
-        final_supplier and the documents related to it. 
+        be delivered. That's the reason why we introduce the notion of
+        final_supplier and the documents related to it.
 
         Purchase Process:
+
         1. Nouveau
             Demandeur:
-            - Nom de la tâche: 
+
+            - Nom de la tâche:
             - Description: produits souhaités
+
                 Nomenclature: Quantité | [référence] | Nom du produit
                 e.g. • 1 | [ABC123] Valve thermostatique
+
             - Tag Destination: Goma, Kinshasa, Bukavu, etc.
             - Sale Order: lien vers la vente liée. Laisser vide si réappro stock
             - Boule verte: Demande complète, prête à l'analyse du responsable achat
-            
+
             Responsable achat:
             - si demande claire et compréhensive: Tag Type: Local ou International
-            - si demande pas claire: boule rouge + Note dans le chatter 
+            - si demande pas claire: boule rouge + Note dans le chatter
 
         2. Consolidation (si International)
             Etant donné que les achats internationaux ne sont faits que
             les jeudi, étape de consolidation pour grouper les commandes
-            
+
             Reponsable achat:
             - Une tâche de consolidation par marque (Victron, Lorentz, etc)
-            - Si une tâche consolidation existe, ajouter 
+            - Si une tâche consolidation existe, ajouter
 
         3. Ordre d'achat
-            Si Achat Local, 
+            Si Achat Local,
 
             Si Achat International,
             Loic prend la tâche en vert dans consolidation
-            
+
 
         4. Fournisseur Final
 
@@ -58,11 +62,11 @@
 
         6. Livraison - en transit
 
-        7. Terminé 
+        7. Terminé
 
-        8. Refusé 
+        8. Refusé
 
-        9. Consolidé        
+        9. Consolidé
 
     """,
 
